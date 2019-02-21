0b750bbb9e4564d7c907c32707f928735c623586
Ilya Titkov <ilya@titkov.me>
2019-02-22 01:14:21 +0500

    fix AnswerCallbackQuery

--- src/requests/answer_callback_query.rs
+++ src/requests/answer_callback_query.rs
@@ -1,12 +1,17 @@
 use requests::Request;
+use std::ops::Not;
 
 #[derive(Serialize, Debug, Clone)]
 pub struct AnswerCallbackQuery {
     pub callback_query_id: String,
-    pub text: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text: Option<String>,
+    #[serde(skip_serializing_if = "Not::not")]
     pub show_alert: bool,
-    pub url: String,
-    pub cache_time: i32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub cache_time: Option<i32>,
 }
 
 impl Request for AnswerCallbackQuery {
